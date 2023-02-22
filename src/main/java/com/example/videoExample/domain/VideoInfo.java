015e4e8dc3716538114b0d7b41aef2daed3e73ab
scy <dbr1214@gmail.com>
2023-02-22 17:27:01 +0900

    Embedde Entity 추가

---
+++ src/main/java/com/example/videoExample/domain/VideoInfo.java
@@ -0,0 +1,44 @@
+package com.example.videoExample.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class VideoInfo {
+
+    private String videoId;
+    private Long fileSize;
+    private int width;
+    private int height;
+//    private String path;
+    private String url;
+
+    public VideoInfo(String videoId, Long fileSize, String url) {
+        this.videoId = videoId;
+        this.fileSize = fileSize;
+        this.url = url;
+    }
+
+    public VideoInfo(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    public VideoInfo(Long fileSize, int width, int height, String url) {
+        this.fileSize = fileSize;
+        this.width = width;
+        this.height = height;
+        this.url = url;
+    }
+
+    public VideoInfo(String videoId, Long fileSize, int width, int height, String url) {
+        this.videoId = videoId;
+        this.fileSize = fileSize;
+        this.width = width;
+        this.height = height;
+        this.url = url;
+    }
+}
